31665a2bce8dd4230dfb356eee69b0311c8fc5cb
bk <wiowou@hotmail.com>
2019-12-22 20:10:15 -0500

    eslint and fontawesome added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import Heading from './components/heading/heading.js';
 import KiwiImage from './components/kiwi-image/kiwi-image.js';
 import 'bootstrap'; //imports the javascript from bootstrap
 import './index.scss';
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 //import React from 'react';
+
+library.add(faSpinner);
+dom.watch(); //will set up an object to watch the dom and dynamically replace any i tags with fontawesome svg tags
 
 const heading = new Heading();
 heading.render('hello world');
