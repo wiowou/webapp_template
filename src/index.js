b702f99abcd0a39b27aceefb37f7bf9eb4493896
abc <>
2019-12-30 23:09:49 -0500

    fontawesome boilerplate

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import Vue from 'vue';
 import App from './components/App.vue';
 import 'bootstrap'; //imports the javascript from bootstrap
 import './index.scss';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+//library.add(faUserSecret);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+export const eventBus = new Vue();
 
 new Vue({
 	el: '#app',
