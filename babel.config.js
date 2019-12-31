0f1902eff1c904381ed4e61d8af677583709824c
abc <>
2019-12-30 22:58:29 -0500

    vue fontawesome

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,7 @@ module.exports = api => {
 		'@babel/plugin-proposal-unicode-property-regex',
 		'@babel/plugin-transform-dotall-regex',
 		['@babel/plugin-proposal-class-properties', { loose: false }],
+		['@babel/plugin-proposal-private-methods', { loose: false }],
 		'@babel/plugin-proposal-json-strings',
 		'@babel/plugin-syntax-dynamic-import',
 		'@babel/plugin-syntax-import-meta'
