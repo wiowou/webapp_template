a93b1fe7433073dfb1e1b7b86d1afb922bda81f2
abc <>
2019-12-30 23:13:04 -0500

    fontawesome boilerplate

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import App from './components/App.vue';
 import 'bootstrap'; //imports the javascript from bootstrap
 import './index.scss';
 import { library } from '@fortawesome/fontawesome-svg-core';
+//import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 //library.add(faUserSecret);
